a19f785c6c400418683ea7b9151defa3a93d32ff
Bike2400 <ebukaechezona8@gmail.com>
2022-05-10 13:42:41 +0100

    Added functionality to the operators and the equal to sign

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const currentDisplayNumber = document.querySelector('.currentNumber');
 const previousDisplayNumber = document.querySelector('.previousNumber');
 
 const equal = document.querySelector('#equal');
+equal.addEventListener('click',evaluate);
 
 const decimal = document.querySelector('#decimal');
 
@@ -26,4 +27,40 @@ function displayNumber(number){
       currentNum += number;
       currentDisplayNumber.textContent = currentNum;
   }
+}
+
+operators.forEach((btn) =>{
+    btn.addEventListener('click',(e) =>{
+        displayOperator(e.target.textContent);
+    });
+});
+
+function displayOperator(op){
+    operator = op;
+    previousNum = currentNum;
+    previousDisplayNumber.textContent = previousNum + ' ' + operator;
+    currentNum = '';
+    currentDisplayNumber.textContent = ''; 
+}
+
+function evaluate(){
+    previousNum = Number(previousNum);
+    currentNum = Number(currentNum);
+
+    if(operator === '+'){
+        previousNum += currentNum;
+    }
+    else if (operator === '-'){
+        previousNum -= currentNum;
+    }
+    else if (operator === '/'){
+        previousNum /= currentNum;
+    }
+    else if (operator === 'x'){
+        previousNum *= currentNum;
+    }
+    previousDisplayNumber.textContent = '';
+    currentDisplayNumber.textContent = previousNum;
+
+
 }
