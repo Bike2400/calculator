3bbe5c1e0ce9bb2b09b824d5d48dd7884ed9246f
Bike2400 <ebukaechezona8@gmail.com>
2022-05-10 15:31:44 +0100

    Added functionality to the decimal button and allows for chaining of functions

--- main.js
+++ main.js
@@ -6,15 +6,21 @@ const currentDisplayNumber = document.querySelector('.currentNumber');
 const previousDisplayNumber = document.querySelector('.previousNumber');
 
 const equal = document.querySelector('#equal');
-equal.addEventListener('click',evaluate);
+equal.addEventListener('click',() =>{
+    if(currentNum != '' && previousNum != '' ){
+        evaluate();
+    }
+});
 
 const decimal = document.querySelector('#decimal');
+decimal.addEventListener('click',addDecimal);
 
 const numberButtons = document.querySelectorAll('#button');
 
 const operators = document.querySelectorAll('#operator');
 
 const clear = document.querySelector('#clear');
+clear.addEventListener('click',clearDisplay);
 
 numberButtons.forEach((btn =>{
     btn.addEventListener('click',(e) => {
@@ -23,10 +29,15 @@ numberButtons.forEach((btn =>{
 }));
 //Function to display the numbers on the screen
 function displayNumber(number){
-  if (currentNum.length <= 10){
-      currentNum += number;
-      currentDisplayNumber.textContent = currentNum;
-  }
+//So that after the first chained result you can continue chaining...So the display wont be stuck at 0
+    if(currentNum != '' && previousNum != '' && operator === ''){ 
+        previousNum = '';
+        currentDisplayNumber.textContent = currentNum;
+    }
+    if (currentNum.length <= 10){
+        currentNum += number;
+        currentDisplayNumber.textContent = currentNum;
+    }
 }
 
 operators.forEach((btn) =>{
@@ -36,11 +47,20 @@ operators.forEach((btn) =>{
 });
 
 function displayOperator(op){
-    operator = op;
-    previousNum = currentNum;
-    previousDisplayNumber.textContent = previousNum + ' ' + operator;
-    currentNum = '';
-    currentDisplayNumber.textContent = ''; 
+    if(previousNum === ''){
+        previousNum = currentNum;
+        operatorCheck(op);
+    }
+    else if (currentNum === ''){
+        operatorCheck(op);
+    }
+    else{
+        evaluate();
+        operator = op;   
+        currentDisplayNumber.textContent = '0';     
+        previousDisplayNumber.textContent = previousNum + ' ' + operator;
+   
+    }
 }
 
 function evaluate(){
@@ -64,6 +84,7 @@ function evaluate(){
     else if (operator === 'x'){
         previousNum *= currentNum;
     }
+    previousNum = roundNumber(previousNum);
     previousNum = previousNum.toString();
     displayResults();
 
@@ -71,12 +92,41 @@ function evaluate(){
 }
 
 function displayResults() {
-    previousDisplayNumber.textContent = '';
-    operator = '';
+  
     if(previousNum.length <= 11){
         currentDisplayNumber.textContent = previousNum;
     }
     else{
         currentDisplayNumber.textContent = previousNum.slice(0,11) + '...';
+    }  
+    previousDisplayNumber.textContent = '';
+    operator = '';
+    currentNum = '';
+}
+
+function roundNumber(num){
+    return Math.round(num * 10000)/10000;
+}
+
+function clearDisplay(){
+    previousNum = '';
+    currentNum = '';
+    previousDisplayNumber.textContent = '';
+    currentDisplayNumber.textContent = 0;
+    operator = '';
+}
+
+//Function to allow you string operations together after pressing =
+function operatorCheck(text){
+    operator = text;
+    previousDisplayNumber.textContent = previousNum + ' ' + operator;
+    currentDisplayNumber.textContent = '0';
+    currentNum = '';
+}
+
+function addDecimal(){
+    if(!currentNum.includes('.')){
+        currentNum += '.';
+        currentDisplayNumber.textContent = currentNum;
     }
 }
